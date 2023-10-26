835e4378e54dbfb8998b08a4cea0eb3d206e116a
murasame29 <oogiriminister@gmail.com>
2023-10-27 01:34:05 +0900

    add: 資格

--- main.go
+++ main.go
@@ -14,5 +14,6 @@ func main() {
 	intro.AddTag(fmt.Sprintf("📫 %sEmail%s", utils.FontColor(35, 134, 54), utils.Reset)).AddContent("oogiriminister@gmail.com")
 	intro.AddTag(fmt.Sprintf("🐦 %sTwitter%s", utils.FontColor(100, 100, 255), utils.Reset)).AddContent("https://twitter.com/fresh_salmon256")
 	intro.AddTag(fmt.Sprintf("🍴 %s好きな食べ物%s", utils.FontColor(100, 100, 255), utils.Reset)).AddContent("🍖 焼き肉、山葵、🍣 寿司")
+	intro.AddTag(fmt.Sprintf("📜 %s保有資格%s", utils.FontColor(100, 100, 255), utils.Reset)).AddContent("DD2種,基本情報,陸特2級")
 	fmt.Println(intro.PrintIntro())
 }
